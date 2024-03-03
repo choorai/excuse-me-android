a09ef4581391b27234f5f1ccd7a67cebb45fcabc
Kim Jae Jun <rlawowns97@naver.com>
2024-03-03 15:45:25 +0900

    #3 : 임시 splash 화면 적용
    - 애니메이션은 적용하지 않음. 적용필요할 경우, MainActivity 수정필요
      - 참고 : https://jdroid.tistory.com/19

--- app/src/main/java/choorai/projects/excuseme/MainActivity.kt
+++ app/src/main/java/choorai/projects/excuseme/MainActivity.kt
@@ -3,13 +3,16 @@ package choorai.projects.excuseme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import choorai.projects.excuseme.ui.theme.ExcuseMeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             ExcuseMeTheme {
+
             }
         }
     }
