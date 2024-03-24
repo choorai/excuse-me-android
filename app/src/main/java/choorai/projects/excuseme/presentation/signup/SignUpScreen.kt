6e66344a9ec943b7c815a796f16a69c4c9ebd8f2
Kim Jae Jun <rlawowns97@naver.com>
2024-03-24 15:11:25 +0900

    #5 : 회원가입 End point 화면 추가

---
+++ app/src/main/java/choorai/projects/excuseme/presentation/signup/SignUpScreen.kt
@@ -0,0 +1,14 @@
+package choorai.projects.excuseme.presentation.signup
+
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun SignUpScreen(navController: NavHostController) {
+    Surface {
+        // TODO : UI 구현 시 작업 예정
+        Text(text = "SignUpScreen")
+    }
+}
